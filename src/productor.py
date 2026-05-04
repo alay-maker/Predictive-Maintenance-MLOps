38727c6ee2ddd5d903a7b8200704386948fd618b
alay-maker <amugica23@gmail.com>
2026-05-04 02:38:30 +0200

    Encapsulate Redis connection and data processing in main
    Refactor productor.py to encapsulate logic in main function and handle exceptions.

--- src/productor.py
+++ src/productor.py
@@ -3,29 +3,28 @@ import time
 import pandas as pd
 import os
 
-host_redis = os.getenv('REDIS_HOST', 'localhost')
+def main():
+    host_redis = os.getenv('REDIS_HOST', 'localhost')
 
-# Conecta con el servidor Redis
+    try:
+        # Conecta con el servidor Redis
+        cliente_redis = redis.Redis(host=host_redis, port=6379, db=0, decode_responses=True)
+        print(f"[PRODUCTOR]: Conectado a servidor Redis. {cliente_redis.ping()}")
 
-cliente_redis = redis.Redis(host=host_redis, port=6379, db=0, decode_responses=True)
-print(f"[PRODUCTOR]: Conectado a servidor Redis. {cliente_redis.ping()}")
+        # Carga datos reservados durante el proceso de entrenamiento
+        df = pd.read_csv('data/processed/datos_sensores_test.csv')
+        test_data = df.drop(columns=['Machine failure'])
 
-# Carga datos reservados durante el proceso de entrenamiento
+        # Simula la sensorización de una fresadora
+        for id, data in test_data.iterrows():
+            # Genera un Redis stream en caso de no existir y añade las mediciones
+            event_id = cliente_redis.xadd(name="input_stream", fields=data.to_dict(), maxlen=10000, approximate=True)
+            time.sleep(1)
 
-df = pd.read_csv('data/processed/datos_sensores_test.csv')
-test_data = df.drop(columns=['Machine failure'])
+    except KeyboardInterrupt:
+        print("\nFresadora detenida manualmente por el usuario.")
+    except Exception as e:
+        print(f"[PRODUCTOR ERROR]: {e}")
 
-# Simula la sensorización de una fresadora
-
-try:
-
-    for id, data in test_data.iterrows():
-
-        # Genera un Redis stream en caso de no existir y añade las mediciones
-        event_id = cliente_redis.xadd(name="input_stream", fields=data.to_dict(), maxlen=10000, approximate=True)
-
-        time.sleep(1)
-
-except KeyboardInterrupt:
-    print("\nFresadora detenida manualmente por el usuario.")
-
+if __name__ == "__main__":
+    main()
