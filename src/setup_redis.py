220ac4af38628bce767ad436435aa149fc2a7891
alay-maker <amugica23@gmail.com>
2026-05-04 21:09:29 +0200

    Update setup_redis.py

--- src/setup_redis.py
+++ src/setup_redis.py
@@ -27,6 +27,8 @@ def preparar_base_datos():
         
         if nodos_antiguos:
             pipe.delete(*nodos_antiguos)
+            print(f"######## Actualizando modelo en Redis ########")
+            print("    - Eliminando {len(nodos_antiguos)} nodos de Redis")
             
         for key, value in diccionario_nodos.items():
             pipe.hset(name=key, mapping=value)
